9ddb5201fa3fe624a0e8053be82664db08168ff6
Steven Marlowe <sdmarlowe@stu.naperville203.org>
2018-09-24 15:08:08 -0500

    cool

--- Chapter 15 Activities/FloodFill/src/Grid.java
+++ Chapter 15 Activities/FloodFill/src/Grid.java
@@ -1,15 +1,25 @@
+import java.util.Scanner;
+import java.util.Stack;
+
 public class Grid
 {
    private static final int SIZE = 10;
    int[][] pixels = new int[SIZE][SIZE];
-   . . .
+   int num = 1;
 
    /**
       Flood fill, starting with the given row and column.
    */
    public void floodfill(int row, int column)
    {
-      . . .
+      Stack<Pair> pairs = new Stack<>();
+      Pair coordinate = new Pair(row, column);
+      pairs.push(coordinate);
+      while(!pairs.empty())
+      {
+         Pair thisCoordinate = pairs.pop();
+         pixels[thisCoordinate.getRow()][thisCoordinate.getCol()] = num;
+      }
    }
 
    @Override
---
+++ Chapter 15 Activities/FloodFill/src/Pair.java
@@ -0,0 +1,20 @@
+public class Pair
+{
+    int row;
+    int col;
+    public Pair(int rowNum, int colNum)
+    {
+        this.row = rowNum;
+        this.col = colNum;
+    }
+
+    public int getRow()
+    {
+        return row;
+    }
+
+    public int getCol()
+    {
+        return col;
+    }
+}
